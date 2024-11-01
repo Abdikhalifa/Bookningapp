613e5720ccd0987a3dc685d4621c5902a241e84f
Abdikani Abdullahi <abdikani.abdullahi@learnet.se>
2024-11-01 11:38:19 +0100

    hej

---
+++ Bookningapp/lokal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookningapp
+{
+    internal class Lokal
+    {
+        public string Namn { get; set; }
+        public string Plats { get; set; }
+        public bool Tillgänglighet { get; set; }
+
+        public Lokal(string namn, string plats, bool tillgänglighet)
+        {
+            Namn = namn;
+            Plats = plats;
+            Tillgänglighet = tillgänglighet;
+        }
+        public virtual void VisaInfo()
+        {
+
+
+        }
+    }
+
+
+}
