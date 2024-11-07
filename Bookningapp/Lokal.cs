2879395230e0d25aafacc9d6d5ec0fdef4947a04
Rebecca Stjärnsten <rebecca.stjarnsten@learnet.se>
2024-11-07 11:52:39 +0100

    mergae från main

--- Bookningapp/Lokal.cs
+++ Bookningapp/Lokal.cs
@@ -6,24 +6,43 @@ using System.Threading.Tasks;
 
 namespace Bookningapp
 {
-    public class Lokal
+    internal class Lokal : IBookable 
     {
+        // Egenskaper för namn och kapacitet
         public string Namn { get; set; }
-        public string Plats { get; set; }
-        public bool Tillgänglighet { get; set; }
+        public int Kapacitet { get; set; }
 
-        public Lokal(string namn, string plats, bool tillgänglighet)
+        //bokningS hanterar bokningarna för lokelen
+        private BokningS bokningS;
+
+        public Lokal(string namn, int kapacitet )
         {
             Namn = namn;
-            Plats = plats;
-            Tillgänglighet = tillgänglighet;
+            Kapacitet = kapacitet;
+            bokningS = new BokningS();
+
+            
         }
         public virtual void VisaInfo()
         {
-
+            Console.WriteLine($"Namn: {Namn} Kapacitet: {Kapacitet}");
 
         }
+        // Metod för att boka en tid
+        public bool Boka(DateTime startTid, DateTime slutTid)
+        {
+            return bokningS.läggTillBokning(startTid, slutTid);
+        }
+        // Metod för att avboka 
+        public bool Avboka(DateTime startTid)
+        {
+            return bokningS.TaBortBokning(startTid);
+           
+        }
+
+       
     }
-
-
 }
+
+
+
