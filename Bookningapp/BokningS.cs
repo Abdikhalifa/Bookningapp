fa61c07b6072cd936ffac33de2271249220d04c0
Abdikani Abdullahi <abdikani.abdullahi@learnet.se>
2024-11-11 19:57:44 +0100

    senaste ändring

--- Bookningapp/BokningS.cs
+++ Bookningapp/BokningS.cs
@@ -266,45 +266,7 @@ namespace Bookningapp
         }
 
 
-        //Metod för att ta bort en bokning(Abdikani)
-        //public static void TaBortBokning()
-        //{
-        //    Console.WriteLine("Ange bokningsnummer att ta bort: ");
-        //    int bokningsNamn = int.Parse(Console.ReadLine());
-        //    BokningS bokning = Bokningar.Find(b => b.Bokningsnummer == bokningsNamn); // Söker i listan efter bokningen med det angivna numret
-
-        //    if (bokning != null) // Kollar om vi hittade en bokning
-        //    {
-        //        Bokningar.Remove(bokning); // Tar bort bokningen från listan
-        //        Console.WriteLine($"Bokning {bokningsNamn} borttagen.");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Bokning hittades inte."); // Om vi inte hittar bokningen, säg till användaren
-        //    }
-        //}
-        // Metod för att uppdatera en bokning (Abdikani)
-        //public static void UppdateraBokning()
-        //{
-        //    Console.WriteLine("Ange bokningsnamn att uppdatera: ");
-        //    int bokningsNamn = int.Parse(Console.ReadLine());
-        //    BokningS bokning = Bokningar.Find(b => b.Bokningsnummer == bokningsNamn);
-
-        //    if (bokning != null)
-        //    {
-        //        Console.WriteLine("Ange ny starttid (YYYY-MM-DD HH:MM): ");
-        //        bokning.StarttidBokning = DateTime.Parse(Console.ReadLine());
-
-        //        Console.WriteLine("Ange ny sluttid (YYYY-MM-DD HH:MM): ");
-        //        bokning.SluttidBokning = DateTime.Parse(Console.ReadLine());
-
-        //        Console.WriteLine($"Bokning {bokningsNamn} uppdaterad till {bokning.StarttidBokning} - {bokning.SluttidBokning}");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Bokning hittades inte.");
-        //    }
-        //}
+       
 
 
     }
